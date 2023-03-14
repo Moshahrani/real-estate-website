2f002d8fa644cb2935713d20a6d88e0ce8a5e119
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-14 14:02:26 -0700

    added company realty logo to NavBar and placement to the very right, with formatted size to render proper dimensions

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -4,31 +4,46 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.png";
 import bayarea from "./bayarea.jpg";
 import cococounty from "./cococounty.jpeg";
+import davisTeam from "./davisTeam.png";
 
 const App = () => {
   return (
     <div>
-      <Container fluid>
-        <Navbar
-          className="navBar"
-          collapseOnSelect
-          expand="lg"
-          bg="transparent"
-          variant=""
-          style={{ fontSize: "1rem", color: "#fff", fontWeight: "bold" }}
-        >
-          <Navbar.Brand href="#home">Shafiq Shahrani</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#pricing">Houses</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#moreInfo">More Info</Nav.Link>
-              <Nav.Link href="contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Container>
+      {/* <Container fluid> */}
+      <Navbar
+        className="navBar"
+        collapseOnSelect
+        expand="lg"
+        style={{
+          backgroundColor: "rgba(0,0,0,0.5)",
+          fontSize: "1rem",
+          color: "#fff",
+          fontWeight: "bold",
+          height: "100px",
+        }}
+      >
+        <Navbar.Brand
+          style={{ paddingLeft: "40px" }}
+          href="#home"
+        >Shafiq Shahrani</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#pricing">Houses</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#moreInfo">More Info</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav>
+          <img
+            src={davisTeam}
+            width="340"
+            height="240"
+            alt="Logo"
+            style={{ position: "fixed", top: -65, right: 0 }}
+          />
+        </Navbar.Collapse>
+      </Navbar>
+      {/* </Container> */}
       <div
         className="background-image"
         style={{ backgroundImage: `url(${cococounty})` }}

ba2cc9fb1b71c4ade950f5b8e0e046f51d27f652
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-14 08:22:22 -0700

    initial commit with create react app code and react bootstrap dependencies with react router as well

---
+++ real-estate/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+  
+};
+export default App;

3be10f6d7819e3a674019a6652b2d4ece95d2e0b
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-15 09:29:44 -0700

    added an opening phrase and opening statement with font styling and text-shadowing, as well as centering

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -53,6 +53,11 @@ const App = () => {
           />
         </Navbar.Collapse>
       </Navbar>
+      <div className="opening-statement">
+        <h1 className="statement">Experience is everything!</h1>
+        {/* <p>This is a paragraph of introductory text.</p> */}
+        <h2 className="mission-statement">Our clients can trust that they receive a customized service that is rooted in our team's extensive industry knowledge and local expertise.</h2>
+      </div>
       {/* </Container> */}
       <div
         className="background-image"

3808031f561bfbea6001c7b4cf17a7d2411899f9
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-15 11:54:40 -0700

    added /About route with bio information as well as moved static Home code to a home functional component with routing. App.js is organized with two routes so far and added two profile pictures, one for bio and the other for the Main page

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -5,65 +5,70 @@ import logo from "./logo.png";
 import bayarea from "./bayarea.jpg";
 import cococounty from "./cococounty.jpeg";
 import davisTeam from "./davisTeam.png";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-      {/* <Container fluid> */}
-      <Navbar
-        className="navBar"
-        collapseOnSelect
-        expand="lg"
-        style={{
-          backgroundColor: "rgba(0,0,0,0.5)",
-          fontSize: "1rem",
-          color: "#fff",
-          fontWeight: "bold",
-          height: "100px",
-        }}
-      >
-        <Navbar.Brand
-          style={{ paddingLeft: "200px" }}
-          href="#home"
-        ></Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          style={{ borderColor: "#fff" }}
-        />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <img
+    <Router>
+      <div>
+        {/* <Container fluid> */}
+        <Navbar
+          className="navBar"
+          collapseOnSelect
+          expand="lg"
+          style={{
+            backgroundColor: "rgba(0,0,0,0.5)",
+            fontSize: "1rem",
+            color: "#fff",
+            fontWeight: "bold",
+            height: "100px",
+          }}
+        >
+          <Navbar.Brand
+            style={{ paddingLeft: "200px" }}
+            href="#home"
+          ></Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            style={{ borderColor: "#fff" }}
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <img
               src={logo}
               width="180"
               height="180"
               alt="Logo"
               style={{ position: "fixed", top: -25, left: 0 }}
             />
-          <Nav className="">
-            <Nav.Link href="#pricing">Houses</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#moreInfo">More Info</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
-          </Nav>
-          <img
-            src={davisTeam}
-            width="340"
-            height="240"
-            alt="Logo"
-            style={{ position: "fixed", top: -65, right: 0, zIndex: 0 }}
-          />
-        </Navbar.Collapse>
-      </Navbar>
-      <div className="opening-statement">
-        <h1 className="statement">Experience is everything!</h1>
-        {/* <p>This is a paragraph of introductory text.</p> */}
-        <h2 className="mission-statement">Our clients can trust that they receive a customized service that is rooted in our team's extensive industry knowledge and local expertise.</h2>
+            <Nav className="">
+              <Nav.Link href="#pricing">Houses</Nav.Link>
+              <Nav.Link href="#moreInfo">More Info</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link href="contact">Contact</Nav.Link>
+            </Nav>
+            <img
+              src={davisTeam}
+              width="340"
+              height="240"
+              alt="Logo"
+              style={{ position: "fixed", top: -65, right: 0, zIndex: 0 }}
+            />
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes>
+          {/* <Route exact path="/" element={< />} /> */}
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+       
+        {/* </Container> */}
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${cococounty})` }}
+        ></div>
       </div>
-      {/* </Container> */}
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${cococounty})` }}
-      ></div>
-    </div>
+    </Router>
   );
 };
 
---
+++ real-estate/src/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    return (
+        <div className="opening-statement">
+        <h1 className="statement">Experience is everything!</h1>
+        {/* <p>This is a paragraph of introductory text.</p> */}
+        <h2 className="mission-statement">
+          Our clients can trust that they receive a customized service that is
+          rooted in our team's extensive industry knowledge and local
+          expertise.
+        </h2>
+      </div>
+    )
+};
+
+export default Home;

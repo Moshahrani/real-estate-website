017346b63b1df4cc3f136770256073e94cd79e4f
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-16 09:54:37 -0700

    put Navbar into its own Customnavbar component, and then created a separate css file for the custom nav bar while updating app.js for better code readability

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,67 +1,14 @@
-import "./App.css";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
-import logo from "./logo.png";
-import bayarea from "./bayarea.jpg";
-import cococounty from "./cococounty.jpeg";
-import davisTeam from "./davisTeam.png";
-import Home from "./Home";
-import About from "./About";
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CustomNavbar from './CustomNavbar';
+import Home from './Home';
+import About from './About';
 
 const App = () => {
-   return (
+  return (
     <Router>
       <div>
-        <Navbar
-          className="navBar"
-          collapseOnSelect
-          expand="lg"
-          style={{
-            backgroundColor: "rgba(0,0,0,0.5)",
-            fontSize: "1rem",
-            color: "#fff",
-            fontWeight: "bold",
-            height: "100px",
-          }}
-        >
-          <Navbar.Brand
-            style={{ paddingLeft: "200px" }}
-            href="#home"
-          ></Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="responsive-navbar-nav"
-            style={{ borderColor: "#fff" }}
-          />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Link to="/">
-              <img
-                
-                src={logo}
-                width="180"
-                height="180"
-                alt="Logo"
-                style={{ position: "fixed", top: -25, left: 0 }}
-                // onClick={handleClick}
-              />
-            </Link>
-            <Nav className="">
-              <Nav.Link href="#pricing">Houses</Nav.Link>
-              <Nav.Link href="#moreInfo">More Info</Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                About
-              </Nav.Link>
-              <Nav.Link href="contact">Contact</Nav.Link>
-            </Nav>
-            <img
-              src={davisTeam}
-              width="340"
-              height="240"
-              alt="Logo"
-              style={{ position: "fixed", top: -65, right: 0, zIndex: 0 }}
-            />
-          </Navbar.Collapse>
-        </Navbar>
+        <CustomNavbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ real-estate/src/CustomNavbar.js
@@ -0,0 +1,60 @@
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import "./CustomNavbar.css";
+import logo from './logo.png';
+import davisTeam from './davisTeam.png';
+
+const CustomNavbar = () => {
+  return (
+    <Navbar
+      className="navBar"
+      collapseOnSelect
+      expand="lg"
+      style={{
+        backgroundColor: "rgba(0,0,0,0.5)",
+        fontSize: "1rem",
+        color: "#fff",
+        fontWeight: "bold",
+        height: "100px",
+      }}
+    >
+      <Navbar.Brand
+        style={{ paddingLeft: "200px" }}
+        href="#home"
+      ></Navbar.Brand>
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        style={{ borderColor: "#fff" }}
+      />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Link to="/">
+          <img
+            src={logo}
+            width="180"
+            height="180"
+            alt="Logo"
+            style={{ position: "fixed", top: -25, left: 0 }}
+            // onClick={handleClick}
+          />
+        </Link>
+        <Nav className="">
+          <Nav.Link href="#pricing">Houses</Nav.Link>
+          <Nav.Link href="#moreInfo">More Info</Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link href="contact">Contact</Nav.Link>
+        </Nav>
+        <img
+          src={davisTeam}
+          width="340"
+          height="240"
+          alt="Logo"
+          style={{ position: "fixed", top: -65, right: 0, zIndex: 0 }}
+        />
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;

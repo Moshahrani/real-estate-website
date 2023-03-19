169b24727df74cee437d130cd7aab631d87d3802
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-19 11:14:23 -0700

    centered background image for home component with enough overlay coverage to cover whole background for clearer rendering of text for both opening statemnent and mission statement

--- real-estate/src/Home.js
+++ real-estate/src/Home.js
@@ -1,47 +1,62 @@
 import cococounty from "./cococounty.jpeg";
 import "./Home.css";
+import ThreeBoxes from "./Three";
 
 const Home = () => {
-    const overlayStyles = {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        minHeight: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.9)",
-        zIndex: 1,
-      };
-  
-    const contentStyles = {
-      position: "relative",
-      zIndex: 2,
-    };
-
-    return (
-      <div className="opening-statement" style={{ position: "relative" }}>
-        <div style={overlayStyles}></div>
-        <div style={contentStyles}>
-          <h1 className="statement">Who you work with matters!</h1>
-          <h2 className="mission-statement">
-            Our clients know they are getting a personalized service based on our
-            local expertise, industry knowledge and expert team.
-          </h2>
-        </div>
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${cococounty})`,
-            // backgroundSize: "cover",
-            // position: "absolute",
-            // width: "100%",
-            // height: "100%",
-            zIndex: 1,
-          }}
-        ></div>
-      </div>
-    );
+  const styles = {
+    backgroundImage: `url(${cococounty})` ,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    //   backgroundRepeat: "no-repeat",
+    height: "100vh",
+    // position: "absolute",
+    // width: "100%",
+    // height: "100%",
+    zIndex: 1,
   };
-  
-  export default Home;
-  
+  const overlayStyles = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "250%",
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
+    zIndex: 1,
+  };
+
+  const contentStyles = {
+    position: "relative",
+    zIndex: 2,
+  };
+
+  return (
+    <div>
+      <div style={styles}>
+        <div className="opening-statement" style={{ position: "relative" }}>
+          <div style={overlayStyles}></div>
+          <div style={contentStyles}>
+            <h1 className="statement">Who you work with matters!</h1>
+            <h2 className="mission-statement">
+              Our clients know they are getting a personalized service based on
+              our local expertise, industry knowledge and expert team.
+            </h2>
+          </div>
+        </div>
+        {/* <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${cococounty})`,
+          height: "100vh",
+          // position: "absolute",
+          // width: "100%",
+          // height: "100%",
+          zIndex: 1,
+        }}
+      ></div> */}
+      </div>
+      {/* <ThreeBoxes /> */}
+    </div>
+  );
+};
+
+export default Home;

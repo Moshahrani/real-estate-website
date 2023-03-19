323fa76d9a31cf900176eda93eca63d090f35a5f
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-19 11:56:54 -0700

    added links for social links including facebook and instagram business profiles

--- real-estate/src/SocialLinks.js
+++ real-estate/src/SocialLinks.js
@@ -1,11 +1,11 @@
-import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import "./SocialLinks.css";
 
 const SocialLinks = () => {
   return (
     <div className="social-container">
-      <a href="https://www.facebook.com"><FaFacebookF /></a>
-      <a href="https://www.instagram.com"><FaInstagram /></a>
+      <a href="https://www.facebook.com/profile.php?id=100090302367422"><FaFacebookF /></a>
+      <a href="https://www.instagram.com/shahrani.realtor/"><FaInstagram /></a>
       {/* <a href="https://twitter.com"><FaTwitter /></a> */}
       <a href="https://www.linkedin.com/in/shafiq-shahrani"><FaLinkedinIn /></a>
     </div>

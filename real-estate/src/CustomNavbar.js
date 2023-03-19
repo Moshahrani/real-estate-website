f02ab7038ae9990d7d944360174c47d2028fc9b0
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-03-19 14:22:56 -0700

    added logic for toggle mode so that when user clicks on toggle at Navbar but decides to increase screen size back to regular navBar menu view, the toggle disappears and the navBar resets when user scales back down after clicking on a different menu option/route

--- real-estate/src/CustomNavbar.js
+++ real-estate/src/CustomNavbar.js
@@ -1,24 +1,38 @@
+import React, { useState, useEffect } from "react";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./CustomNavbar.css";
-import logo from './logo.png';
-import davisTeam from './davisTeam.png';
+import logo from "./logo.png";
+import davisTeam from "./davisTeam.png";
 
-// const CustomNavbar = () => {
-//   return (
-//     <Navbar
-//       className="navBar"
-//       collapseOnSelect
-//       expand="lg"
-//       style={{
-//         backgroundColor: "rgba(0,0,0,0.5)",
-//         fontSize: "1rem",
-//         color: "#fff",
-//         fontWeight: "bold",
-//         height: "100px",
-//       }}
-//     >
 const CustomNavbar = ({ background }) => {
+  const [toggle, setToggle] = useState(false);
+  const [routeChange, setRouteChange] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setToggle(false);
+
+      }
+    }; 
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleRouteChange = () => {
+    setToggle(false);
+  }
+
   const styles = {
     backgroundImage: `url(${background})`,
     backgroundColor: "rgba(0,0,0,0.5)",
@@ -42,7 +56,8 @@ const CustomNavbar = ({ background }) => {
       ></Navbar.Brand>
       <Navbar.Toggle
         aria-controls="responsive-navbar-nav"
-        style={{ borderColor: "#fff" }}
+        style={{ borderColor: "#fff", position: "absolute", left: "10px" }}
+        onClick={handleToggle}
       />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Link to="/">
@@ -51,26 +66,36 @@ const CustomNavbar = ({ background }) => {
             width="180"
             height="180"
             alt="Logo"
-            style={{ position: "fixed", top: -25, left: 0 }}
-            // onClick={handleClick}
+            style={{
+              position: "fixed",
+              top: -25,
+              left: 0,
+              display: toggle ? "none" : "block",
+            }}
           />
         </Link>
-        <Nav className="navOptions ml-auto">
-          <Nav.Link href="#pricing">Houses</Nav.Link>
-          <Nav.Link href="#moreInfo">More Info</Nav.Link>
-          <Nav.Link as={Link} to="/about">
+        <Nav className="navOptions ml-auto" >
+          <Nav.Link href="#pricing" onClick={handleRouteChange}>Houses</Nav.Link>
+          <Nav.Link href="#moreInfo" onClick={handleRouteChange}>More Info</Nav.Link>
+          <Nav.Link as={Link} to="/about" onClick={handleRouteChange}>
             About
           </Nav.Link>
-          <Nav.Link href="contact">Contact</Nav.Link>
+          <Nav.Link href="contact" onClick={handleRouteChange}>Contact</Nav.Link>
         </Nav>
         <a href="https://www.davisteamexp.com">
-        <img
-          src={davisTeam}
-          width="340"
-          height="240"
-          alt="Logo"
-          style={{ position: "fixed", top: -65, right: 0, zIndex: 0 }}
-        />
+          <img
+            src={davisTeam}
+            width="340"
+            height="240"
+            alt="Logo"
+            style={{
+              position: "fixed",
+              top: -65,
+              right: 0,
+              zIndex: 0,
+              display: toggle ? "none" : "block",
+            }}
+          />
         </a>
       </Navbar.Collapse>
     </Navbar>

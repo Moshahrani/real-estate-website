c05b21b1d7171812fcc737c2a7d56f44addd1450
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-05-21 09:49:11 -0700

    added responsive to smaller screen, border and box shadow on all grid items and added margins plus justifying items/content for better user experience

--- real-estate/src/components/HouseList.js
+++ real-estate/src/components/HouseList.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../css/HouseList.css";
 
 const HouseList = () => {
@@ -8,25 +8,25 @@ const HouseList = () => {
   useEffect(() => {
     const fetchHouseData = async () => {
       const options = {
-        method: 'GET',
+        method: "GET",
         url: process.env.REACT_APP_ZILLOW_URL,
-    params: {
-        location: 'Walnut Creek , ca',
-        page: '1',
-        home_type: 'Houses'
-      },
-      headers: {
-              'X-RapidAPI-Key': process.env.REACT_APP_ZILLOW_API_KEY,
-              'X-RapidAPI-Host': process.env.REACT_APP_ZILLOW_HOST,
-            },
-    };
+        params: {
+          location: "Walnut Creek , ca",
+          page: "1",
+          home_type: "Houses",
+        },
+        headers: {
+          "X-RapidAPI-Key": process.env.REACT_APP_ZILLOW_API_KEY,
+          "X-RapidAPI-Host": process.env.REACT_APP_ZILLOW_HOST,
+        },
+      };
 
       try {
         const response = await axios.request(options);
-        console.log("success")
+        console.log("success");
         setHouseData(response.data.props);
       } catch (error) {
-        console.error('Error fetching house data:', error);
+        console.error("Error fetching house data:", error);
       }
     };
 
@@ -35,16 +35,25 @@ const HouseList = () => {
 
   return (
     <div className="grid-container">
-      {houseData && houseData.map((house) => (
-        <div key={house.zpid} className="grid-item">
-          <h2>{house.address}</h2>
-          <p>Price: {house.price}</p>
-          <p>Bedrooms: {house.bedrooms}</p>
-          <p>Bathrooms: {house.bathrooms}</p>
-          <p>Living Area: {house.livingArea}</p>
-          <img src={house.imgSrc} alt={house.address} />
-        </div>
-      ))}
+      {houseData &&
+        houseData.map((house) => (
+          <div key={house.zpid} className="grid-item">
+            <img
+              src={house.imgSrc}
+              alt={house.address}
+              className="grid-image"
+            />
+            <p>{house.address}</p>
+            <p>Price: {house.price}</p>
+            <div className="listing-info">
+              <p>Bedrooms: {house.bedrooms}</p>
+              <div className="divider"></div>
+              <p>Bathrooms: {house.bathrooms}</p>
+              <div className="divider"></div>
+              <p>Living Area: {house.livingArea}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };

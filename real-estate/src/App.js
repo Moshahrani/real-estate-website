723c82db50f82f2b068b5b56401ec9f8fa5b928a
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-05-21 21:05:08 -0700

    removed previous navbar code due to rendering issues during hamburger menu dropdown and found out that react-boostrap code and custom react state code were not working together, so restarted with a simple navbar component layout for proper UI functionality

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <CustomNavbar background={city} />
+        <CustomNavbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/" element={<Home />} />
--- real-estate/src/CustomNavbar.js
+++ real-estate/src/CustomNavbar.js
@@ -5,97 +5,137 @@ import ".//css/CustomNavbar.css";
 import logo from ".//media/logo.png";
 import davisTeam from ".//media/davisTeam.png";
 
-const CustomNavbar = ({ background }) => {
-  const [toggle, setToggle] = useState(false);
-
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
-
-  // const styles = {
-  //   backgroundImage: `url(${background})`,
-  //   backgroundColor: "rgba(0,0,0,0.5)",
-  //   backgroundSize: "cover",
-  //   height: "100px",
-  //   fontSize: "1rem",
-  //   color: "#fff",
-  //   fontWeight: "bold",
-  // };
-  const styles = {
-    backgroundColor:  "#333", // Change the background color here
-    backgroundSize: "cover",
-    height: "100px",
-    fontSize: "1rem",
-    color: "#fff",
-    fontWeight: "bold",
-  };
+const CustomNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <Navbar
-      className="navBar"
-      // collapseOnSelect
       expand="lg"
-      style={{ ...styles }}
+      expanded={expanded}
+      onToggle={setExpanded}
+      bg="dark"
+      variant="dark"
     >
-      <Navbar.Brand style={{ paddingLeft: "200px" }}></Navbar.Brand>
-      <Navbar.Toggle
-        // aria-controls="responsive-navbar-nav"
-        style={{ borderColor: "#fff", position: "absolute", left: "10px" }}
-        onClick={handleToggle}
-      />
-      <Navbar.Collapse
-        id="responsive-navbar-nav"
-        className={toggle ? "show-menu" : "hide"}
-      >
-        {/* <Nav.Link href="/">
-          <img
-            src={logo}
-            width="180"
-            height="180"
-            alt="Logo"
-            style={{
-              position: "fixed",
-              top: -25,
-              left: 0,
-              display: toggle ? "none" : "block",
-            }}
-          />
-        </Nav.Link> */}
-        <Nav className="navOptions ml-auto">
-          <Nav.Link as={Link} to="/">
+      <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+        <img src={davisTeam} className="custom-logo" alt="Brand Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/houses">
+          <Nav.Link as={Link} to="/houses" onClick={() => setExpanded(false)}>
             Houses
           </Nav.Link>
-          <Nav.Link as={Link} to="/moreInfo">
+          <Nav.Link as={Link} to="/moreInfo" onClick={() => setExpanded(false)}>
             More Info
           </Nav.Link>
-          <Nav.Link as={Link} to="/about">
+          <Nav.Link as={Link} to="/about" onClick={() => setExpanded(false)}>
             About
           </Nav.Link>
-          <Nav.Link as={Link} to="contact">
+          <Nav.Link as={Link} to="/contact" onClick={() => setExpanded(false)}>
             Contact
           </Nav.Link>
         </Nav>
-        <a href="https://www.davisteamexp.com">
-          <img
-            src={davisTeam}
-            width="340"
-            height="240"
-            alt="Logo"
-            style={{
-              position: "fixed",
-              top: -65,
-              right: 0,
-              zIndex: 0,
-              display: toggle ? "none" : "block",
-            }}
-          />
-        </a>
       </Navbar.Collapse>
     </Navbar>
   );
 };
 
 export default CustomNavbar;
+
+// const CustomNavbar = () => {
+//   const [toggle, setToggle] = useState(false);
+
+//   const handleToggle = () => {
+//     setToggle(!toggle);
+//   };
+
+//   // const styles = {
+//   //   backgroundImage: `url(${background})`,
+//   //   backgroundColor: "rgba(0,0,0,0.5)",
+//   //   backgroundSize: "cover",
+//   //   height: "100px",
+//   //   fontSize: "1rem",
+//   //   color: "#fff",
+//   //   fontWeight: "bold",
+//   // };
+//   const styles = {
+//     backgroundColor: "#333", // Change the background color here
+//     backgroundSize: "cover",
+//     height: "100px",
+//     fontSize: "1rem",
+//     color: "#fff",
+//     fontWeight: "bold",
+//   };
+
+//   return (
+//     <Navbar
+//       className="navBar"
+//       // collapseOnSelect
+//       expand="lg"
+//       style={{ ...styles }}
+//     >
+//       <Navbar.Brand style={{ paddingLeft: "200px" }}></Navbar.Brand>
+//       <Navbar.Toggle
+//         // aria-controls="responsive-navbar-nav"
+//         style={{ borderColor: "#fff", position: "absolute", left: "10px" }}
+//         onClick={handleToggle}
+//       />
+//       <Navbar.Collapse
+//         id="responsive-navbar-nav"
+//         className={toggle ? "show-menu" : "hide"}
+//       >
+//         {/* <Nav.Link href="/">
+//           <img
+//             src={logo}
+//             width="180"
+//             height="180"
+//             alt="Logo"
+//             style={{
+//               position: "fixed",
+//               top: -25,
+//               left: 0,
+//               display: toggle ? "none" : "block",
+//             }}
+//           />
+//         </Nav.Link> */}
+//         <Nav className="navOptions ml-auto">
+//           <Nav.Link as={Link} to="/" onClick={() => setToggle(false)}>
+//             Home
+//           </Nav.Link>
+//           <Nav.Link as={Link} to="/houses" onClick={() => setToggle(false)}>
+//             Houses
+//           </Nav.Link>
+//           <Nav.Link as={Link} to="/moreInfo" onClick={() => setToggle(false)}>
+//             More Info
+//           </Nav.Link>
+//           <Nav.Link as={Link} to="/about" onClick={() => setToggle(false)}>
+//             About
+//           </Nav.Link>
+//           <Nav.Link as={Link} to="contact" onClick={() => setToggle(false)}>
+//             Contact
+//           </Nav.Link>
+//         </Nav>
+//         <a href="https://www.davisteamexp.com">
+//           <img
+//             src={davisTeam}
+//             width="340"
+//             height="240"
+//             alt="Logo"
+//             style={{
+//               position: "fixed",
+//               top: -65,
+//               right: 0,
+//               zIndex: 0,
+//               display: toggle ? "none" : "block",
+//             }}
+//           />
+//         </a>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// };
+
+// export default CustomNavbar;

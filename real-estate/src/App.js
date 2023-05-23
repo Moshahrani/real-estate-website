59c942546381b43b8244a923f656aaeec4ab0edc
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-05-23 09:47:50 -0700

    added contact to each route that needed it and removed it from rendering in all routes

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CustomNavbar from './CustomNavbar';
-import Home from './components/Home';
-import About from './components/About';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CustomNavbar from "./CustomNavbar";
+import Home from "./components/Home";
+import About from "./components/About";
 import ContactForm from "./components/ContactForm";
 import Contact from "./components/Contact";
 import city from ".//media/city.jpg";
 import SocialLinks from "./components/SocialLinks";
 import MoreInfo from "./components/MoreInfo";
 import Houses from "./components/Houses";
-import HouseList from "./components/HouseList"
+import HouseList from "./components/HouseList";
 // import Associations from "./Associations";
 
 const App = () => {
@@ -18,7 +18,15 @@ const App = () => {
       <div>
         <CustomNavbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <Contact />
+              </>
+            }
+          />
           <Route
             path="/houses"
             element={
@@ -27,13 +35,34 @@ const App = () => {
               </HouseList>
             }
           />
-          <Route path="/moreInfo" element={<MoreInfo />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactForm />} />
+          <Route
+            path="/moreInfo"
+            element={
+              <>
+                <MoreInfo />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <About />
+                <Contact />
+              </>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <ContactForm />
+                <Contact />
+              </>
+            }
+          />
         </Routes>
-        <div className="d-none d-lg-block">
-          <Contact />
-        </div>
+        <div className="d-none d-lg-block"></div>
         {/* <Associations /> */}
         <SocialLinks />
       </div>

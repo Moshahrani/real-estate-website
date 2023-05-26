d1db76d47c2a9e05a753390fdebbf6aaba7de6b6
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-05-26 13:45:47 -0700

    added new personal photo and removed animation delay for the mission statement and statement css animations, added a border between images as well in home component

--- real-estate/src/components/Home.js
+++ real-estate/src/components/Home.js
@@ -1,10 +1,11 @@
 import cococounty from "../media/cococounty.jpeg";
 import "../css/Home.css";
 import Associations from "./Associations";
+import personal1 from "../media/personal1.jpg";
 
 const Home = () => {
   const styles = {
-    backgroundImage: `url(${cococounty})` ,
+    backgroundImage: `url(${cococounty})`,
     backgroundSize: "cover",
     backgroundPosition: "center center",
     //   backgroundRepeat: "no-repeat",
@@ -13,6 +14,12 @@ const Home = () => {
     // width: "100%",
     // height: "100%",
     zIndex: 1,
+  };
+
+  const personal = {
+    backgroundImage: `url(${personal1})`,
+     height: "800px",
+     maxWidth: "600px"
   };
   const overlayStyles = {
     position: "absolute",
@@ -33,7 +40,7 @@ const Home = () => {
     <div>
       <div style={styles}>
         <div className="opening-statement" style={{ position: "relative" }}>
-          <div style={overlayStyles}></div>
+          {/* <div style={overlayStyles}></div> */}
           <div style={contentStyles}>
             <h1 className="statement">Who you work with matters!</h1>
             <h2 className="mission-statement">
@@ -42,18 +49,13 @@ const Home = () => {
             </h2>
           </div>
         </div>
-        {/* <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${cococounty})`,
-          height: "100vh",
-          // position: "absolute",
-          // width: "100%",
-          // height: "100%",
-          zIndex: 1,
-        }}
-      ></div> */}
       </div>
+      <div className="whitespace-container">
+      <div className="whitespace-border">
+        <div className="whitespace-text"></div>
+      </div>
+    </div>
+      <div style={personal}></div>
       <Associations />
     </div>
   );

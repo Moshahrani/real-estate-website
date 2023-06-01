0344296e612d7949b5ce5c188389bc832f3ed752
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-01 07:33:55 -0700

    added sticky option for navbar to scroll down with user and also added logic for opacity to help readability for navbar and content as user scrolls down the page

--- real-estate/src/CustomNavbar.js
+++ real-estate/src/CustomNavbar.js
@@ -7,15 +7,34 @@ import davisTeam from ".//media/davisTeam-2.png";
 
 const CustomNavbar = () => {
   const [expanded, setExpanded] = useState(false);
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let scrolled = document.scrollingElement.scrollTop;
+      if (scrolled > 50) { 
+        setOpacity(0.7);
+      } else {
+        setOpacity(1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <Navbar
-      className="custom-navbar"
+      className="custom-navbar sticky-top"
       expand="lg"
       expanded={expanded}
       onToggle={setExpanded}
       // bg="dark"
       variant="dark"
+      style={{ opacity: opacity }}
     >
       <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
         <img src={logo2} className="personal-logo" alt="Personal Logo" />
@@ -46,98 +65,3 @@ const CustomNavbar = () => {
 };
 
 export default CustomNavbar;
-
-// const CustomNavbar = () => {
-//   const [toggle, setToggle] = useState(false);
-
-//   const handleToggle = () => {
-//     setToggle(!toggle);
-//   };
-
-//   // const styles = {
-//   //   backgroundImage: `url(${background})`,
-//   //   backgroundColor: "rgba(0,0,0,0.5)",
-//   //   backgroundSize: "cover",
-//   //   height: "100px",
-//   //   fontSize: "1rem",
-//   //   color: "#fff",
-//   //   fontWeight: "bold",
-//   // };
-//   const styles = {
-//     backgroundColor: "#333", // Change the background color here
-//     backgroundSize: "cover",
-//     height: "100px",
-//     fontSize: "1rem",
-//     color: "#fff",
-//     fontWeight: "bold",
-//   };
-
-//   return (
-//     <Navbar
-//       className="navBar"
-//       // collapseOnSelect
-//       expand="lg"
-//       style={{ ...styles }}
-//     >
-//       <Navbar.Brand style={{ paddingLeft: "200px" }}></Navbar.Brand>
-//       <Navbar.Toggle
-//         // aria-controls="responsive-navbar-nav"
-//         style={{ borderColor: "#fff", position: "absolute", left: "10px" }}
-//         onClick={handleToggle}
-//       />
-//       <Navbar.Collapse
-//         id="responsive-navbar-nav"
-//         className={toggle ? "show-menu" : "hide"}
-//       >
-//         {/* <Nav.Link href="/">
-//           <img
-//             src={logo}
-//             width="180"
-//             height="180"
-//             alt="Logo"
-//             style={{
-//               position: "fixed",
-//               top: -25,
-//               left: 0,
-//               display: toggle ? "none" : "block",
-//             }}
-//           />
-//         </Nav.Link> */}
-//         <Nav className="navOptions ml-auto">
-//           <Nav.Link as={Link} to="/" onClick={() => setToggle(false)}>
-//             Home
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="/houses" onClick={() => setToggle(false)}>
-//             Houses
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="/moreInfo" onClick={() => setToggle(false)}>
-//             More Info
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="/about" onClick={() => setToggle(false)}>
-//             About
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="contact" onClick={() => setToggle(false)}>
-//             Contact
-//           </Nav.Link>
-//         </Nav>
-//         <a href="https://www.davisteamexp.com">
-//           <img
-//             src={davisTeam}
-//             width="340"
-//             height="240"
-//             alt="Logo"
-//             style={{
-//               position: "fixed",
-//               top: -65,
-//               right: 0,
-//               zIndex: 0,
-//               display: toggle ? "none" : "block",
-//             }}
-//           />
-//         </a>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-
-// export default CustomNavbar;

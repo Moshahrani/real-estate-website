45324fb5cef6444a972e8bad2ce54f3578f5c67d
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-05 09:12:10 -0700

    added two backyard image options for carousel's background and fixed resizing issue for carousel during mobile view and overall padding of containers and image sizing withn the photo carousel component

--- real-estate/src/components/Carousel.js
+++ real-estate/src/components/Carousel.js
@@ -3,58 +3,63 @@ import { Carousel } from "react-bootstrap";
 import personal1 from "../media/personal1.jpg";
 import "../css/Carousel.css";
 import house3 from "../media/house3.jpeg";
-import house2 from "../media/house2.jpeg";
-import interior from "../media/interior1.jpeg";
 import house4 from "../media/house4.jpeg";
 import house5 from "../media/house5.jpeg";
 import house6 from "../media/house6.jpeg";
 import house7 from "../media/house7.webp";
-// import Image1 from './path-to-image1.jpg';
-// import Image2 from './path-to-image2.jpg';
-// import Image3 from './path-to-image3.jpg';
+import backyardFence from "../media/backyardFence.jpeg";
+
+const carouselBackground = {
+  backgroundImage: `url(${backyardFence})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+  height: '50%'
+};
 
 const CustomCarousel = () => {
   return (
-    <div className="image-carousel-container">
-      <img src={personal1} alt="" className="side-image" />
-      <div className="carousel-content">
-        <Carousel className="custom-carousel">
-          <Carousel.Item className="custom-carousel-item">
-            <img
-              className="d-block w-100 custom-carousel-img"
-              src={house6}
-              alt="house image"
-            />
-          </Carousel.Item>
-          <Carousel.Item className="custom-carousel-item">
-            <img
-              className="d-block w-100 custom-carousel-img"
-              src={house3}
-              alt="house image "
-            />
-          </Carousel.Item>
-          <Carousel.Item className="custom-carousel-item">
-            <img
-              className="d-block w-100 custom-carousel-img"
-              src={house4}
-              alt="house image"
-            />
-          </Carousel.Item>
-          <Carousel.Item className="custom-carousel-item">
-            <img
-              className="d-block w-100 custom-carousel-img"
-              src={house5}
-              alt="house image"
-            />
-          </Carousel.Item>
-          <Carousel.Item className="custom-carousel-item">
-            <img
-              className="d-block w-100 custom-carousel-img"
-              src={house7}
-              alt="house image"
-            />
-          </Carousel.Item>
-        </Carousel>
+    <div style={carouselBackground}>
+      <div className="image-carousel-container">
+        <img src={personal1} alt="" className="side-image" />
+        <div className="carousel-content">
+          <Carousel className="custom-carousel">
+            <Carousel.Item className="custom-carousel-item">
+              <img
+                className="d-block w-100 custom-carousel-img"
+                src={house6}
+                alt="house image"
+              />
+            </Carousel.Item>
+            <Carousel.Item className="custom-carousel-item">
+              <img
+                className="d-block w-100 custom-carousel-img"
+                src={house3}
+                alt="house image "
+              />
+            </Carousel.Item>
+            <Carousel.Item className="custom-carousel-item">
+              <img
+                className="d-block w-100 custom-carousel-img"
+                src={house4}
+                alt="house image"
+              />
+            </Carousel.Item>
+            {/* <Carousel.Item className="custom-carousel-item">
+              <img
+                className="custom-carousel-img"
+                src={house5}
+                alt="house image"
+              />
+            </Carousel.Item> */}
+            {/* <Carousel.Item className="custom-carousel-item">
+              <img
+                className="d-block w-100 custom-carousel-img"
+                src={house7}
+                alt="house image"
+              />
+            </Carousel.Item> */}
+          </Carousel>
+        </div>
       </div>
     </div>
   );

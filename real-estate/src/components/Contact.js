2fe4025c124e5deba6ea530c87699dc2a774a601
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-13 09:11:02 -0700

    added new contact component for contact route with css fiel for initial design/styling

--- real-estate/src/components/Contact.js
+++ real-estate/src/components/Contact.js
@@ -1,32 +1,15 @@
-import { FaUser, FaPhone, FaEnvelope, FaIdCard, FaMapMarkerAlt } from 'react-icons/fa';
+import React from "react";
+import ContactForm from "./ContactForm";
 import "../css/Contact.css";
 
 const Contact = () => {
-  return (
-    <div className="container contact-container">
-    <div className="row">
-        <div className="col-md-6">
-          <div className="form-group">
-            <FaUser /> <div>Shafiq Shahrani</div>
-          </div>
-          <div className="form-group">
-            <FaPhone /> <div>9254052719</div>
-          </div>
-          <div className="form-group">
-            <FaEnvelope /> <div>shahranihomes@gmail.com</div>
-          </div>
+    return (
+        <div className="contact-container">
+            <div className="contact-form-contact-route">
+            <ContactForm />
+            </div>
         </div>
-        <div className="col-md-6">
-          <div className="form-group">
-            <FaIdCard /> <div>DRE #02192359</div>
-          </div>
-          <div className="form-group">
-            <FaMapMarkerAlt/> <div>2890 N Main Street,<br></br> Walnut Creek, CA</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default Contact;

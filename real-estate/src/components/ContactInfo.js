bce938d4e17b90fdf9f4cdea199093608b5429d0
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-13 09:11:48 -0700

    component with css now has contact info from previous component/location

---
+++ real-estate/src/components/ContactInfo.js
@@ -0,0 +1,32 @@
+import { FaUser, FaPhone, FaEnvelope, FaIdCard, FaMapMarkerAlt } from 'react-icons/fa';
+import "../css/ContactInfo.css";
+
+const ContactInfo = () => {
+  return (
+    <div className="container contact-container">
+    <div className="row">
+        <div className="col-md-6">
+          <div className="form-group">
+            <FaUser /> <div>Shafiq Shahrani</div>
+          </div>
+          <div className="form-group">
+            <FaPhone /> <div>9254052719</div>
+          </div>
+          <div className="form-group">
+            <FaEnvelope /> <div>shahranihomes@gmail.com</div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form-group">
+            <FaIdCard /> <div>DRE #02192359</div>
+          </div>
+          <div className="form-group">
+            <FaMapMarkerAlt/> <div>2890 N Main Street,<br></br> Walnut Creek, CA</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;

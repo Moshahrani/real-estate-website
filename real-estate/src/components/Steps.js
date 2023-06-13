68f663d14c673953211aef7551770c9151c9b167
Moshfeq Shahrani <moshfeqshahrani@gmail.com>
2023-06-13 09:06:11 -0700

    removed logo stock images and now using human stock images as replacment

--- real-estate/src/components/Steps.js
+++ real-estate/src/components/Steps.js
@@ -2,21 +2,24 @@ import "../css/Steps.css";
 import Step1 from "../media/Step1.png";
 import Step2 from "../media/Step2.png";
 import Step3 from "../media/Step3.png";
+import Office1 from "../media/office1.jpg";
+import Office2 from "../media/office2.jpg";
+import Office3 from "../media/office3.jpg";
 
 const Steps = () => {
   return (
     <div className="step-container">
     <div className="item">
-        <img src={Step1} alt="Description 1" />
-        <p>I'll evaluate your property and craft a custom sales strategy.</p>
+        <img src={Office1} alt="Description 1" />
+        <p>We evaluate your home thoroughly and create a custom sales strategy to maximize its value and attract qualified buyers.</p>
     </div>
     <div className="item">
-        <img src={Step2} alt="Description 2" />
-        <p>I'll negotiate effectively to secure competitive offers.</p>
+        <img src={Office2} alt="Description 2" />
+        <p>Our team excels in effective negotiation, working diligently to secure competitive offers that maximize your financial gain and guarantee a successful outcome in the sale of your property.</p>
     </div>
     <div className="item">
-        <img src={Step3} alt="Description 3" />
-        <p>I'll manage all closing formalities for a smooth transaction.</p>
+        <img src={Office3} alt="Description 3" />
+        <p>Our meticulous team handles all closing formalities with attention to detail, ensuring a seamless transaction from start to finish.</p>
     </div>
 </div>
   );
